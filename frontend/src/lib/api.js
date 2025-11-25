8b3a20b4a11c4b5d4364dee64d7e11191568db15
Akash <akashpapasani@gmail.com>
2025-11-26 01:35:11 +0530

    Fix package.json syntax error and update API URL

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:7000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const emailAPI = {
   getAll: () => axios.get(`${API_BASE_URL}/emails`),
