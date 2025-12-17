208403b5af5dc6949bb8f9039e6b18ca5aec5f61
PAPASANI NARENDRA SAI <137531548+NarendraPapasani@users.noreply.github.com>
2025-12-18 00:53:36 +0530

    Update email.controller.js

--- backend/controllers/email.controller.js
+++ backend/controllers/email.controller.js
@@ -1,5 +1,5 @@
 import prisma from "../utils/prisma.js";
-import connectAI from "../utils/connectAI.js";
+import connectAI from "../utils/genAI.js";
 import dotenv from "dotenv";
 
 dotenv.config();
